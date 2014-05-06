88a329ca46db755cc951a78bf01f63441e7fcbf0
EC2 Default User <ec2-user@ip-10-161-91-232.us-west-1.compute.internal>
2014-05-06 17:52:45 +0000

    add support for blessings terminal control rewrite print function to use absolute terminal coords

--- tailcolor.py
+++ tailcolor.py
@@ -6,19 +6,21 @@ import time
 class heatmap():
     def __init__(self, text, scale=1):
         self.text = text
-        self.time = int(time.time())
+        self.time = int(time.time()*5)
         self.scale = scale
 
     def age(self):
-        return (int(time.time()) - self.time)
+        return (int(time.time()*5) - self.time)
 
     def redfade(self, scale=None):
         if scale:
             scaler = scale
         else:
             scaler = self.scale
-        r = 255
-        g = min((self.age()*scaler), 255)
+        fade = max(min(200 - self.age()*scaler,0),-120)
+        #print fade
+        r = 255 + fade
+        g = min((self.age()*scaler), 255) + fade
         b = g
         color = '#{0:02x}{1:02x}{2:02x}'.format(r,g,b)
         return color
@@ -48,25 +50,66 @@ if __name__ == '__main__':
     import select
     import tailcolor
     import sys
+    import os
+    import blessings
 
     f = subprocess.Popen(['tail','-F',sys.argv[1]],\
             stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     p = select.poll()
     p.register(f.stdout)
 
-    lines = []
+    lines = ['-']
     print ""
+    term = blessings.Terminal()
 
     try:
+        os.system('setterm -cursor off')
         while True:
-            if p.poll(1):
-                if len(lines) > 60:
-                    _ = lines.pop(0)
-                lines.append(tailcolor.heatmap(f.stdout.readline()))
+            #print 'main'
+            ready = p.poll(1)
+            line = ''
+            #print ready
+            if ready:
+                if ready[0][1] == select.POLLHUP:
+                    #print 'POLLHUP'
+                    break
+                if ready[0][1] == select.POLLIN:
+                    while ready[0][1] & select.POLLIN:
+                        #print 'poll1', ready,len(lines)
+                        #if len(lines) > 44440:
+                        #    _ = lines.pop(0)
+                        char = f.stdout.read(1)
+                        line += char
+                        #print line
+                        if char == '\n': 
+                            if len(lines)>term.height-1: lines.pop(0)
+                            lines.append(heatmap(line,1))
+                            line = ''
+                            break
+                    #lines.append(tailcolor.heatmap(f.stdout.read(1), scale=20))
             else:
-                time.sleep(1)
-            print "\n" * 60
-            for line in lines:
-                print line,
+                pass
+		#print 'poll0'
+                #time.sleep(0.1)
+            #print 'clearing screen'
+            time.sleep(0.1)
+            #if len(lines) < 30: print "\n" * 30 
+            #print 'printing lines'
+            #print len(lines)
+            for x in range(term.height-1):
+                with term.location(0, x): 
+                    #      if len(lines)>18: time.sleep(0.5)
+                    try:
+                       # print x,
+                        if x < len(lines):
+                            print str(lines[x+1]).replace('\n','') + term.clear_eol
+                      #      time.sleep(0.1)
+                        else:
+            #                print x,
+             #               time.sleep(0.1)
+                            print term.clear_eol
+                    except IndexError:
+                        print term.clear_eol
+            #sys.stdout.flush()
     except KeyboardInterrupt:
-        pass
+        os.system('setterm -cursor on')
