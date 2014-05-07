c8708e1a0b5091f5adea94b606bab690ef393680
EC2 Default User <ec2-user@ip-10-161-91-232.us-west-1.compute.internal>
2014-05-07 01:03:31 +0000

    increment version to publish to PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tailcolor',
-    version='0.1',
+    version='0.1a',
     description="'tail -F' with color feedback based on how recent the line was seen",
     url='https://github.com/bVector/tailcolor',
     author='bVector',
